903fea70e834be2105b12d1d4ccd3ddbbd70be13
SUN <378826926@qq.com>
2024-03-12 17:01:06 +0800

    swy,feat:init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 启动入口
+ */
+
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/Apitest.java
@@ -0,0 +1,11 @@
+package cn.bugstack.chatbot.api.test;
+
+/**
+ * @author   swy
+ * @description   单元测试
+ */
+
+public class Apitest {
+}
+
+
